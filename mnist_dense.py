fixed issue with weight initializations and weight scaling

mnist_dense.py
@@ -20,7 +20,7 @@ def dSigmoid(h):
 
 # preparing data
 mnist = fetch_mldata('MNIST original', data_home='./')
-# create a list of random indices for traing set
+# create a list of random indices for training set
 train_idx = np.random.choice(len(mnist.data), training_set_size, replace=False)
 # create x and y by picking samples from the random indices
 mnist_x = np.array([mnist.data[i] for i in train_idx])
@@ -33,7 +33,7 @@ print(mnist_x.shape)
 # One hot encoding
 y_onehot = torch.zeros([training_set_size, D_out]).type(dtype)
 y_onehot.scatter_(1, mnist_y, 1.0)
-
+mnist_x /= 255  # scaling down x to fall between 0 and 1
 x = torch.cat((mnist_x, torch.ones([training_set_size, 1])), 1)  # adding biases
 
 x_batches = torch.split(x, batch_size)
@@ -41,12 +41,12 @@ y_batches = torch.split(mnist_y, batch_size)
 y_onehot_batches = torch.split(y_onehot, batch_size)
 
 # Randomly initialize weights
-w1 = torch.randn(D_in + 1, H).type(dtype)
-w2 = torch.randn(H + 1, D_out).type(dtype)
+w1 = torch.randn(D_in + 1, H).type(dtype) / np.sqrt(D_in + 1)
+w2 = torch.randn(H + 1, D_out).type(dtype) / np.sqrt(H + 1)
 
 learning_rate = 0.007
-epochs = 10
-f, (ax1, ax2, ax3) = plt.subplots(3, 1, sharey=True)
+epochs = 1
+f, (ax1, ax2) = plt.subplots(2, 1, sharey=True)
 
 for t in range(epochs):
     num_of_batches = int(training_set_size / batch_size)
@@ -74,17 +74,9 @@ for t in range(epochs):
         # expanded_dEdh allows us to look at various kinds of distributions which get “summed-away” in backprop.
         delta_y_broadcastable = delta_y.unsqueeze(1)
         expanded_dEdh = torch.mul(delta_y_broadcastable, w2[:-1, :])
+        dSig_h_broadcastable = dSigmoid(h).unsqueeze(-1)
+        delta_h_expanded = torch.mul(expanded_dEdh, dSig_h_broadcastable)
 
-        # Experiment
-        if (t == 0 or t == (epochs - 1)) and b == (num_of_batches - 1):
-            hist_min = -0.05#torch.min(expanded_dEdh[:, 1, 1])*1.5
-            hist_max = 0.05 #torch.max(expanded_dEdh[:, 1, 1])*1.5
-            ax1.hist(torch.mean(expanded_dEdh, dim=0)[1, :].numpy(), bins=51, range=(hist_min, hist_max), histtype='step')
-            ax2.hist(torch.mean(expanded_dEdh, dim=0)[2, :].numpy(), bins=51, range=(hist_min, hist_max), histtype='step')
-            ax3.hist(torch.mean(expanded_dEdh, dim=0)[3, :].numpy(), bins=51, range=(hist_min, hist_max), histtype='step')
-            #plt.show(block=(b == num_of_batches - 1))
-            #plt.draw()
-            hist_test = torch.histc(expanded_dEdh[:, 1, 1], bins=50, min= torch.min(expanded_dEdh), max=torch.max(expanded_dEdh), out=None)
 
         weight_scaling = 1
         if False:
@@ -102,13 +94,20 @@ for t in range(epochs):
             weight_scaling = expanded_dEdh_sum / expanded_dEdh_adjusted_sum
             # learning_rate = 0.01
 
-        dEdh = torch.sum(expanded_dEdh, dim=2).mul(weight_scaling)
-        #old_dEdh = delta_y.mm(w2[:-1, :].t())
-        delta_h = torch.mul(dEdh,  dSigmoid(h))
+        delta_h = torch.sum(delta_h_expanded, dim=2).mul(weight_scaling)  # calculates delta for each heuron in the hidden layer for each training sample
         grad_w1 = x_batches[b].t().mm(delta_h)
         # Update weights using gradient descent
         w1 -= learning_rate * grad_w1
         w2 -= learning_rate * grad_w2
+
+        hist_min = -0.025  # torch.min(expanded_dEdh[:, 1, 1])*1.5
+        hist_max = 0.025  # torch.max(expanded_dEdh[:, 1, 1])*1.5
+        if t == 0 and b == 0:
+            delta_h_means = torch.mean(delta_h_expanded, dim=0)  # calculates the batch's average delta for each weight from hidden to output layer
+            _, max_index = torch.max(delta_h_means[0], dim=0)  #for neuron 0 in hidden layer, it find the index of the weight with max delta
+            _, min_index = torch.min(delta_h_means[0], dim=0)  #for neuron 0 in hidden layer, it find the index of the weight with min delta
+            ax1.hist(delta_h_expanded[:, 0, max_index[0]].numpy(), bins=101, range=(hist_min, hist_max), histtype='step')
+            ax2.hist(delta_h_expanded[:, 0, min_index[0]].numpy(), bins=101, range=(hist_min, hist_max), histtype='step')
 
 print('training done')
 